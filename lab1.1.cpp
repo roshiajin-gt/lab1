16080f6303f6f6751a63519e4b233f261a6f0697
user <user@ROSHIAJIN>
2025-09-26 21:04:53 +0300

    Добавьте файлы проекта.

---
+++ lab1.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    cout << "размеры типов данных в байтах" << endl;
+    cout << "int: " << sizeof(int) << " байт" << endl;
+    cout << "short int: " << sizeof(short int) << " байт" << endl;
+    cout << "long int: " << sizeof(long int) << " байт" << endl;
+    cout << "float: " << sizeof(float) << " байт" << endl;
+    cout << "double: " << sizeof(double) << " байт" << endl;
+    cout << "long double: " << sizeof(long double) << " байт" << endl;
+    cout << "char: " << sizeof(char) << " байт" << endl;
+    cout << "bool: " << sizeof(bool) << " байт" << endl;
+
+    return 0;
+}
